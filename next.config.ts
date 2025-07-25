d194d3ede8df01944cc53be345950d3cb4236d85
Tony <tonyjays@yahoo.com>
2025-07-25 07:35:25 +0100

    effected some configurations

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "standalone",
+  // any other options you want
 };
 
 export default nextConfig;
